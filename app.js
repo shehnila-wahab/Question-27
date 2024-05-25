1de991c3d79ea5f775670de3ba63a711e9325977
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 23:15:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var alien_color = "green";
+if (alien_color = "green") {
+    console.log("the player earned 5  points");
+}
+else if (alien_color = "yellow") {
+    console.log("the player earned 10 points");
+}
+else if (alien_color = "red") {
+    console.log("the player earned 15 points");
+}
---
+++ app.ts
@@ -0,0 +1,9 @@
+let alien_color: string="green"
+if(alien_color="green"){
+    console.log("the player earned 5  points");
+}
+else if(alien_color="yellow"){
+    console.log("the player earned 10 points")
+}else if(alien_color="red"){
+    console.log("the player earned 15 points")
+}
